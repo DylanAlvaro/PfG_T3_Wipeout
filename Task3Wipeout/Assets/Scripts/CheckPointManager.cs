dc762184e4b507df9e2f95496d9350f25643dbce
Dylan Alvaro <dylanalvaro495@gmail.com>
2023-03-01 10:37:32 +1100

    made changes

--- Task3Wipeout/Assets/Scripts/CheckPointManager.cs
+++ Task3Wipeout/Assets/Scripts/CheckPointManager.cs
@@ -1,24 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CheckPointManager : MonoBehaviour
 {
 
-    public GameObject CheckPoint;
-
+   
+    public GameObject UI;
+    
     private Vector3 spawn;
-    
-    
-    // Start is called before the first frame update
-    void Start()
+
+
+    private void Start()
     {
-        spawn = gameObject.transform.position;
+        UI.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            UI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        UI.SetActive(false);
     }
 }
