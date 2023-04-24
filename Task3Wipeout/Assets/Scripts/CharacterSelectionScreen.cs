a251ae9919e8cbda7ebf9853479cf372d5010417
Dylan Alvaro <s213919@students.aie.edu.au>
2023-04-24 16:45:39 +1000

    making changes to wipeout

--- Task3Wipeout/Assets/Scripts/CharacterSelectionScreen.cs
+++ Task3Wipeout/Assets/Scripts/CharacterSelectionScreen.cs
@@ -5,12 +5,8 @@ using UnityEngine.UI;
 
 public class CharacterSelectionScreen : MonoBehaviour
 {
-    public Material changeMat;
-    public Button btn;
-
-    public void ChangeMaterial()
-    {
-        Renderer renderer = GetComponent<Renderer>();
-        renderer.material = changeMat;
-    }
+    public Material[] material;
+    private MeshRenderer renderer;
+    
+    
 }
