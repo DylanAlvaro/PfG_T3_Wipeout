066a38509c40512eb0259945b6c472ed211bceb9
Dylan Alvaro <s213919@students.aie.edu.au>
2023-10-17 11:57:05 +1100

    ready for final submission

--- Task3Wipeout/Assets/Scripts/RaycastImpulse.cs
+++ Task3Wipeout/Assets/Scripts/RaycastImpulse.cs
@@ -12,35 +12,54 @@ public class RaycastImpulse : MonoBehaviour
     public Ray ray;
     public Vector3 offset;
     private Camera _camera;
-
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-    
-    private void OnTriggerEnter(Collider other)
-   {
-       if (other.CompareTag("Door"))
-       {
-           if (Input.GetMouseButtonDown(0))
-           {
-               Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
 
-               if (Physics.Raycast(ray, out RaycastHit hit))
-               {
-                   if(hit.collider.gameObject.GetComponent<Door>() != null)
-                   {
-                       Vector3 distToTarget = hit.point - transform.position;
-                       Vector3 forceDir = distToTarget.normalized;
-                       
-                       rb.AddForce(forceDir * hitForce, ForceMode.Impulse);
-                   }
-               }
-           }
-       }
-   }
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitPoint;
+
+            if (Physics.Raycast(ray, out hitPoint, 500))
+            {
+                if (hitPoint.collider.gameObject.GetComponent<PennBall>() != null)
+                {
+                    Rigidbody rb = hitPoint.collider.GetComponent<Rigidbody>();
+                    if (rb != null)
+                        rb.AddForce(ray.direction * hitForce);
+                }
+            }
+        }
+    }
+    
+   // private void OnTriggerEnter(Collider other)
+   //{
+   //    //if (other.CompareTag("Door"))
+   //    //{
+   //        if (Input.GetMouseButtonDown(0))
+   //        {
+   //            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+   //
+   //            if (Physics.Raycast(ray, out RaycastHit hit))
+   //            {
+   //                if(hit.collider.gameObject.GetComponent<Door>() != null)
+   //                {
+   //                    Vector3 distToTarget = hit.point - transform.position;
+   //                    Vector3 forceDir = distToTarget.normalized;
+   //                    rb.AddForce(forceDir * hitForce, ForceMode.Impulse);
+   //                }
+   //            }
+   //        }
+   //    //}
+   //}
 
 
    private void OnDrawGizmos()
